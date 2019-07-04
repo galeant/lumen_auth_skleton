2cd5c98ee0731bbf005a51b5ed9c81129f57d859
ilham <ilham.fadillah@gmail.com>
2019-07-04 16:05:30 +0700

    init

--- app/Http/Controllers/ExampleController.php
+++ app/Http/Controllers/ExampleController.php
@@ -2,6 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use Lcobucci\JWT\Builder;
+use Lcobucci\JWT\Parser;
+
+use Lcobucci\JWT\Signer\Key;
+use Lcobucci\JWT\Signer\Hmac\Sha256;
+
 class ExampleController extends Controller
 {
     /**
@@ -14,5 +20,43 @@ class ExampleController extends Controller
         //
     }
 
+    public function index(){
+        // $time = time();
+        // $token = (new Builder())->issuedBy('http://example.com') // Configures the issuer (iss claim)
+        //                         ->permittedFor('http://example.org') // Configures the audience (aud claim)
+        //                         ->identifiedBy('4f1g23a12aa', true) // Configures the id (jti claim), replicating as a header item
+        //                         ->issuedAt($time) // Configures the time that the token was issue (iat claim)
+        //                         ->canOnlyBeUsedAfter($time + 60) // Configures the time that the token can be used (nbf claim)
+        //                         ->expiresAt($time + 3600) // Configures the expiration time of the token (exp claim)
+        //                         ->withClaim('uid', 1) // Configures a new claim, called "uid"
+        //                         ->getToken(); // Retrieves the generated token
+        // dd($token);
+
+        // $token->getHeaders(); // Retrieves the token headers
+        // $token->getClaims(); // Retrieves the token claims
+
+        // // $token = '{ayam.bebek.cicak}';
+        // // $token = (new Parser())->parse((string) $token); // Parses from a string
+        // // $token->getHeaders(); // Retrieves the token header
+        // // $token->getClaims(); // Retrieves the token claims
+
+        // return [$token->getHeader('jti'),$token->getClaim('iss'),$token->getClaim('uid'),$token]; // will print "4f1g23a12aa"
+        $signer = new Sha256();
+        $time = time();
+
+        $token = (new Builder())->issuedBy('http://example.com') // Configures the issuer (iss claim)
+                                ->permittedFor('http://example.org') // Configures the audience (aud claim)
+                                ->identifiedBy('4f1g23a12aa', true) // Configures the id (jti claim), replicating as a header item
+                                ->issuedAt($time) // Configures the time that the token was issue (iat claim)
+                                ->canOnlyBeUsedAfter($time + 60) // Configures the time that the token can be used (nbf claim)
+                                ->expiresAt($time + 3600) // Configures the expiration time of the token (exp claim)
+                                ->withClaim('uid', 1) // Configures a new claim, called "uid"
+                                ->getToken($signer, new Key('testing')); // Retrieves the generated token
+
+
+        var_dump($token->verify($signer, 'testing 1')); // false, because the key is different
+        var_dump($token->verify($signer, 'testing')); // true, because the key is the same
+        
+    }
     //
 }
--- app/Http/Middleware/ExampleMiddleware.php
+++ app/Http/Middleware/ExampleMiddleware.php
@@ -3,6 +3,10 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Lcobucci\JWT\Parser;
+use Lcobucci\JWT\Builder;
+use Lcobucci\JWT\ValidationData;
+use Lcobucci\JWT\Signer\Hmac\Sha256;
 
 class ExampleMiddleware
 {
@@ -15,6 +19,51 @@ class ExampleMiddleware
      */
     public function handle($request, Closure $next)
     {
+        if (! $request->user() && session()->has('token')) {
+            session()->forget('token');
+        } elseif ($request->user() && (! session('token') || ! $this->validate($request, session('token')))) {
+            session()->put('token', $this->issue($request, $minutes));
+        }
         return $next($request);
+    }
+    /**
+     * Issue the token.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int|null  $minutes
+     * @return string
+     */
+    protected function issue($request, $minutes)
+    {
+        $builder = (new Builder)
+            ->setId(str_random())
+            ->setIssuer($request->getHost())
+            ->setAudience($request->getHost())
+            ->setSubject($request->user()->id);
+
+        if ($minutes) {
+            $builder->setExpiration(time() + ($minutes * 60));
+        }
+
+        return (string) $builder->sign(new Sha256, config('app.key'))->getToken();
+    }
+
+    /**
+     * Validate the token.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  string  $token
+     * @return bool
+     */
+    protected function validate($request, $token)
+    {
+        $token = (new Parser)->parse($token);
+
+        $data = new ValidationData;
+        $data->setIssuer($request->getHost());
+        $data->setAudience($request->getHost());
+        $data->setSubject($request->user()->id);
+
+        return $token->validate($data);
     }
 }
